c9d150814825907f878f8685e08139571631cfb0
Rinku kumar saini <rinkukumarsaini9799118495@gmail.com>
2025-11-24 23:35:36 +0530

    workflow stup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
-  res.send({ message: `server is running on by cicd pipleline v3` });
+  res.send({ message: `server is running on by cicd pipleline v4` });
 });
 app.listen(port, () => {
   console.log(`app running on the http://localhost:${port}`);
